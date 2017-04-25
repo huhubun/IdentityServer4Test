af7f9c5777f80237593e88188a51c7bd9856c23e
huhubun <huhubun@users.noreply.github.com>
2017-04-25 23:36:39 +0800

    Revert "Implement save client info to db"

--- Server.Admin/Controllers/ClientController.cs
+++ Server.Admin/Controllers/ClientController.cs
@@ -1,12 +1,10 @@
-using AutoMapper;
 using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Server.Admin.Mappers;
 using Server.Admin.Models.ClientViewModels;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Server.Admin.Controllers
@@ -30,8 +28,7 @@ namespace Server.Admin.Controllers
 
         public IActionResult Add()
         {
-            var model = new ClientViewModel();
-            return View(model);
+            return View();
         }
 
         [HttpPost]
@@ -48,99 +45,14 @@ namespace Server.Admin.Controllers
         {
             var client = _configurationDbContext.Clients.Include(c => c.AllowedGrantTypes).Single(c => c.Id == id);
 
-            var model = client.ToModel();
-
-            return View(model);
-
-
-
-            //return View(Mapper.Map<ClientViewModel>(client.ToModel()));
+            return View(client.ToModel() as ClientViewModel);
         }
 
         [HttpPost]
-        public IActionResult Edit(ClientViewModel client)
+        public IActionResult Edit(Client client)
         {
-            var clientEntity = _configurationDbContext.Clients.Include(c => c.AllowedGrantTypes).Single(c => c.Id == client.Id);
-
-            clientEntity = Mapper.Map<ClientViewModel, Client>(client, clientEntity);
-
             return View();
-
-            //var clientEntity = _configurationDbContext.Clients.Find(client.Id);
-
-            //clientEntity.ClientName = client.ClientName;
-
-            //_configurationDbContext.SaveChanges();
-
-            //return RedirectToAction(nameof(Edit), new { id = clientEntity.Id });
         }
-
-
-        #region ClientScopes
-
-        [HttpGet]
-        public IActionResult CreateScope(int clientId)
-        {
-            var model = new CreateScopeViewModel { ClientId = clientId };
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult CreateScope(CreateScopeViewModel model)
-        {
-            var entity = model.ToEntity();
-
-            var client = _configurationDbContext.Clients.Include(c => c.AllowedScopes).Single(c => c.Id == model.ClientId);
-            client.AllowedScopes.Add(entity);
-            _configurationDbContext.SaveChanges();
-
-            return RedirectToAction(nameof(Edit), new { id = model.ClientId });
-        }
-
-        [HttpGet]
-        public IActionResult DeleteScope(int clientId, int id)
-        {
-            var scopeSet = _configurationDbContext.Set<ClientScope>();
-            scopeSet.Remove(scopeSet.Find(id));
-            _configurationDbContext.SaveChanges();
-
-            return RedirectToAction(nameof(Edit), new { id = clientId });
-        }
-
-        #endregion
-
-        #region ClientSecret
-
-        [HttpGet]
-        public IActionResult CreateSecret(int clientId)
-        {
-            var model = new CreateSecretViewModel { ClientId = clientId };
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult CreateSecret(CreateSecretViewModel model)
-        {
-            var entity = model.ToEntity();
-
-            var client = _configurationDbContext.Clients.Include(c => c.ClientSecrets).Single(c => c.Id == model.ClientId);
-            client.ClientSecrets.Add(entity);
-            _configurationDbContext.SaveChanges();
-
-            return RedirectToAction(nameof(Edit), new { id = model.ClientId });
-        }
-
-        [HttpGet]
-        public IActionResult DeleteSecret(int clientId, int id)
-        {
-            var secretSet = _configurationDbContext.Set<ClientSecret>();
-            secretSet.Remove(secretSet.Find(id));
-            _configurationDbContext.SaveChanges();
-
-            return RedirectToAction(nameof(Edit), new { id = clientId });
-        }
-
-        #endregion
 
     }
 }
